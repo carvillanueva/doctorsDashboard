df2a5a44022b15d3cbcf8b07220d1e7d57eb775c
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-07-13 16:59:43 -0400

    udpate fileuploader file

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,21 +1,22 @@
 "use client";
 
-import { convertFileToUrl } from "@/lib/utils";
 import Image from "next/image";
 import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 
+import { convertFileToUrl } from "@/lib/utils";
+
 type FileUploaderProps = {
   files: File[] | undefined;
-  onChanage: (files: File[]) => void;
+  onChange: (files: File[]) => void;
 };
 
-const FileUploader = ({ files, onChanage }: FileUploaderProps) => {
+export const FileUploader = ({ files, onChange }: FileUploaderProps) => {
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    // Do something with the files
-    onChanage(acceptedFiles);
+    onChange(acceptedFiles);
   }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
     <div {...getRootProps()} className="file-upload">
@@ -23,36 +24,30 @@ const FileUploader = ({ files, onChanage }: FileUploaderProps) => {
       {files && files?.length > 0 ? (
         <Image
           src={convertFileToUrl(files[0])}
-          height={1000}
           width={1000}
-          alt="upload image"
+          height={1000}
+          alt="uploaded image"
           className="max-h-[400px] overflow-hidden object-cover"
         />
       ) : (
         <>
           <Image
             src="/assets/icons/upload.svg"
-            height={40}
             width={40}
+            height={40}
             alt="upload"
           />
           <div className="file-upload_label">
-            <p className="text-14-regular">
-              <span className="text-green-500">Click to upload</span> or drag
-              and drop
+            <p className="text-14-regular ">
+              <span className="text-green-500">Click to upload </span>
+              or drag and drop
             </p>
-            <p>SVG, PNG, JPG, Or Gif (max 800x400) </p>
+            <p className="text-12-regular">
+              SVG, PNG, JPG or GIF (max. 800x400px)
+            </p>
           </div>
         </>
       )}
-
-      {/* {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )} */}
     </div>
   );
 };
-
-export default FileUploader;
