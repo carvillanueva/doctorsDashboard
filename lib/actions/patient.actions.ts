045230982bf6f0c5c850ae2b229f3a767114c3db
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-09-08 09:01:30 -0400

    setup otp for ad access

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,15 +1,16 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-import { InputFile } from "node-appwrite/file";
+// DOUBLE CHECK INPUTFILE ERROR
+import { InputFile } from "node-appwrite";
 
 import {
   BUCKET_ID,
   DATABASE_ID,
-  databases,
   ENDPOINT,
   PATIENT_COLLECTION_ID,
   PROJECT_ID,
+  databases,
   storage,
   users,
 } from "../appwrite.config";
