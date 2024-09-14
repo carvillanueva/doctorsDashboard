9b1343fd4d1dc58a355b337c344271826dc5a31a
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-09-14 18:19:05 -0400

    create skelton data table

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,14 @@
+import DataTable from "@/components/table/DataTable";
 import StatCard from "@/components/StatCard";
+import columns from "@/components/table/Columns";
+import { getRecentAppointmentsList } from "@/lib/actions/appointment.actions";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Admin = () => {
+const Admin = async () => {
+  const appointments = await getRecentAppointmentsList();
+
   return (
     <section className="mx-auto flex max-w-7xl flex-col space-y-14">
       <header className="admin-header">
@@ -29,23 +34,25 @@ const Admin = () => {
         <section className="admin-stat">
           <StatCard
             type="appointments"
-            count={5}
+            count={appointments.scheduledCount}
             label="Scheduled Appointments"
             icon="/assets/icons/appointments.svg"
           />
           <StatCard
             type="pending"
-            count={10}
+            count={appointments.pendingCount}
             label="Pending Appointments"
             icon="/assets/icons/pending.svg"
           />
           <StatCard
             type="cancelled"
-            count={2}
+            count={appointments.cancelledCount}
             label="Cancelled Appointments"
             icon="/assets/icons/cancelled.svg"
           />
         </section>
+
+        <DataTable columns={columns} data={appointments.documents}  />
       </main>
     </section>
   );
