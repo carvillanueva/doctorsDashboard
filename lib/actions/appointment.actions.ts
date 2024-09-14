71e781d299c95ebb568cf55b2f502321386f86ba
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-09-14 18:14:31 -0400

    create route to collect all status apps

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,12 +1,13 @@
 "use server"
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
   try {
@@ -37,3 +38,40 @@ export const getAppointment = async (appointmentId: string) => {
     console.log("Error: An error occurred while fetching the appointment:", error);
   }
 };
+
+// GET ALL APPOINTMENTS FOR ADMIN STAT CARD
+export const getRecentAppointmentsList = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc("$createdAt")],
+    );
+    const initailCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    }
+
+    const counts = (appointments.documents as Appointment[]).reduce((acc, curr) => {
+      if (curr.status === "scheduled") {
+        acc.scheduledCount += 1;
+      } else if (curr.status === "pending") {
+        acc.pendingCount += 1;
+      } else if (curr.status === "cancelled") {
+        acc.cancelledCount += 1;
+      }
+      return acc;
+    }, initailCounts);
+
+    const data = {
+      totalCount: appointments.total,
+      ...counts,
+      documents: appointments.documents,
+    }
+
+    return parseStringify(data);
+  } catch (error) {
+    console.error("An error occurred while fetching recent appointments:", error);
+  }
+}
